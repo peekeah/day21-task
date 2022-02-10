927df8885a849c25014315099f747b0fddae69da
Pranay Kothari <pranay.kothari109@gmail.com>
2022-02-10 11:50:29 +0530

    input

---
+++ script.js
@@ -0,0 +1,25 @@
+let arr=['one','two','three','four','five','six','seven','eight','nine'];
+let arr_2=['twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninty']
+let n=123;
+if(n<100)
+{
+ let x=n%10;
+ 
+}
+else
+{
+    if(n<1000)
+    {
+        for(i=0;i<3;i++)
+        {
+
+        }
+
+    }
+    else
+    {
+
+
+    }
+}
+
